e2c171662360d9472282f6ad858dcedcf3b7e65a
Chloe <github@chloepetta.net>
2026-03-07 08:07:07 -0500

    switched to list of resources for easier management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,19 @@
 terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.97.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
-    }
-  }
+	required_providers {
+		proxmox = {
+			source  = "bpg/proxmox"
+			version = "0.97.0"
+		}
+		local = {
+			source  = "hashicorp/local"
+			version = "2.4.0"
+		}
+	}
 }
 
 variable "local" {
-  type = bool
-  default = false
+	type = bool
+	default = false
 }
 
 variable "cipassword" {}
@@ -26,73 +26,52 @@ variable "pm_pasword" {}
 variable "tailscale_auth_key" {}
 
 variable "gateway_ip" {}
-variable "pm_ips" {}
-variable "dns_ips" {}
 
-variable "pm_nodes" {
-  type = list(string)
-  default = [
-    "pm1",
-    "pm2",
-    "pm3",
-  ]
-}
-
-variable dns_count {
-  type = number
-  default = 2
-}
-
-variable dns_target_nodes {
-  type = list(string)
-  default = [
-    "pm2",
-    "pm3",
-  ]
-}
+variable "pm_node_list" {}
+variable "dns_server_list" {}
 
 variable "pfs1_ip" {}
 
 provider "proxmox" {
-  endpoint = var.local ? var.pm_api_url : var.pm_api_url_remote
-  api_token = var.pm_api_token
-  username = "root@pam"
-  password = var.pm_pasword
-  insecure = true
-  
-  ssh {
-    username = "root"
-    password = var.pm_pasword
-    private_key = file("../ssh/private_key")
-    agent    = true
+	endpoint = var.local ? var.pm_api_url : var.pm_api_url_remote
+	api_token = var.pm_api_token
+	username = "root@pam"
+	password = var.pm_pasword
+	insecure = true
+	
+	ssh {
+		username = "root"
+		password = var.pm_pasword
+		private_key = file("../ssh/private_key")
+		agent    = true
 
-    node {
-      name    = "pm1"
-      address = var.local ? var.pm_ips[0] : "pm1"
-    }
-    node {
-      name    = "pm2"
-      address = var.local ? var.pm_ips[1] : "pm2"
-    }
-    node {
-      name    = "pm3"
-      address = var.local ? var.pm_ips[2] : "pm3"
-    }
-  }
+		node {
+			name    = var.pm_node_list[0].name
+			address = var.local ? var.pm_node_list[0].ip_address : var.pm_node_list[0].name
+		}
+		node {
+			name    = var.pm_node_list[1].name
+			address = var.local ? var.pm_node_list[1].ip_address : var.pm_node_list[1].name
+		}
+		node {
+			name    = var.pm_node_list[2].name
+			address = var.local ? var.pm_node_list[2].ip_address : var.pm_node_list[2].name
+		}
+	}
 }
 
 #-------------------------------------------------------
 # Cloud Image Resources
 #-------------------------------------------------------
 resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  count        = 3
-  content_type = "import"
-  datastore_id = "local"
-  node_name    = var.pm_nodes[count.index]
-  url          = "https://cloud-images.ubuntu.com/minimal/releases/noble/release/ubuntu-24.04-minimal-cloudimg-amd64.img"
-  file_name    = "ubuntu-24.04-minimal-cloudimg-amd64.img.qcow2" # rename to *.qcow2 for import
-  overwrite    = false
-  overwrite_unmanaged = true
+	count        = length(var.pm_node_list)
+	content_type = "import"
+	datastore_id = "local"
+	node_name    = var.pm_node_list[count.index].name
+	url          = "https://cloud-images.ubuntu.com/minimal/releases/noble/release/ubuntu-24.04-minimal-cloudimg-amd64.img"
+	file_name    = "ubuntu-24.04-minimal-cloudimg-amd64.img.qcow2" # rename to *.qcow2 for import
+	overwrite    = false
+	overwrite_unmanaged = true
 }
 
 #-------------------------------------------------------
@@ -100,104 +79,104 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
 #-------------------------------------------------------
 // Reference https://atxfiles.netgate.com/mirror/downloads/
 resource "proxmox_virtual_environment_download_file" "mint_iso_1" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pm1"
-  url          = "https://mirrors.edge.kernel.org/linuxmint/stable/22.3/linuxmint-22.3-xfce-64bit.iso"
-  file_name    = "linuxmint-22.3-xfce-64bit.iso"
-  overwrite    = false
-  overwrite_unmanaged = true
-  checksum = "45a835b5dddaf40e84d776549e0b19b3fbd49673b6cc6434ebddbfcd217df776"
-  checksum_algorithm = "sha256"
+	content_type = "iso"
+	datastore_id = "local"
+	node_name    = "pm1"
+	url          = "https://mirrors.edge.kernel.org/linuxmint/stable/22.3/linuxmint-22.3-xfce-64bit.iso"
+	file_name    = "linuxmint-22.3-xfce-64bit.iso"
+	overwrite    = false
+	overwrite_unmanaged = true
+	checksum = "45a835b5dddaf40e84d776549e0b19b3fbd49673b6cc6434ebddbfcd217df776"
+	checksum_algorithm = "sha256"
 }
 
 #-------------------------------------------------------
 # DNS
 #-------------------------------------------------------
 resource "local_file" "dns_snippet" {
-  count = var.dns_count
-  content = templatefile("${path.module}/cloud-init/templates/common.tftpl", {
-    hostname           = "dns${count.index+1}"
-    tailscale_auth_key = var.tailscale_auth_key
-    cipassword_hash    = var.cipassword_hash
-    ssh_public_key     = var.ssh_public_key
-  })
-  filename = "${path.module}/cloud-init/tmp/cloud_config_dns${count.index+1}.yml"
+	count = length(var.dns_server_list)
+	content = templatefile("${path.module}/cloud-init/templates/common.tftpl", {
+		hostname           = "dns${count.index+1}"
+		tailscale_auth_key = var.tailscale_auth_key
+		cipassword_hash    = var.cipassword_hash
+		ssh_public_key     = var.ssh_public_key
+	})
+	filename = "${path.module}/cloud-init/tmp/cloud_config_dns${count.index+1}.yml"
 }
 
 resource "proxmox_virtual_environment_file" "dns_cloud_config" {
-  count = var.dns_count
-  depends_on  = [resource.local_file.dns_snippet]
-  content_type = "snippets"
-  datastore_id = "local"
-  node_name    = var.dns_target_nodes[count.index]
-  source_file {
-    path = resource.local_file.dns_snippet[count.index].filename
-  }
+	count = length(var.dns_server_list)
+	depends_on  = [resource.local_file.dns_snippet]
+	content_type = "snippets"
+	datastore_id = "local"
+	node_name    = var.dns_server_list[count.index].host_node
+	source_file {
+		path = resource.local_file.dns_snippet[count.index].filename
+	}
 }
 
 resource "proxmox_virtual_environment_vm" "dns" {
-  count       = var.dns_count
-  # vm_id       = 101
-  name        = "dns${count.index+1}"
-  node_name   = var.dns_target_nodes[count.index]
-  description = "Managed by Terraform"
-  tags        = ["terraform", "ubuntu"]
-  started = true
-  on_boot = true
-  reboot_after_update = true
+	count       = length(var.dns_server_list)
+	# vm_id       = 101
+	name        = "dns${count.index+1}"
+	node_name   = var.dns_server_list[count.index].host_node
+	description = "Managed by Terraform"
+	tags        = ["terraform", "ubuntu"]
+	started = true
+	on_boot = true
+	reboot_after_update = true
 
-  cpu {
-    cores = 1
-    type  = "host"
-  }
-  memory {
-    dedicated = 2048
-    floating  = 2048 # set equal to dedicated to enable ballooning
-  }
-  disk {
-    datastore_id = "local-lvm"
-    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image[1].id
-    interface    = "scsi0"
-    discard      = "on"
-    size         = 10
-  }
-  
-  initialization {
-    datastore_id = "local-lvm"
-    user_data_file_id   = proxmox_virtual_environment_file.dns_cloud_config[count.index].id
+	cpu {
+		cores = 1
+		type  = "host"
+	}
+	memory {
+		dedicated = 2048
+		floating  = 2048 # set equal to dedicated to enable ballooning
+	}
+	disk {
+		datastore_id = "local-lvm"
+		import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image[1].id
+		interface    = "scsi0"
+		discard      = "on"
+		size         = 10
+	}
+	
+	initialization {
+		datastore_id = "local-lvm"
+		user_data_file_id   = proxmox_virtual_environment_file.dns_cloud_config[count.index].id
 
-    ip_config {
-      ipv4 {
-        address = "${var.dns_ips[count.index]}/24"
-        gateway = var.gateway_ip
-      }
-    }
-    dns {
-      servers = ["9.9.9.9", "1.1.1.1",  "1.0.0.1"]
-    }
-  }
+		ip_config {
+			ipv4 {
+				address = "${var.dns_server_list[count.index].ip_address}/24"
+				gateway = var.gateway_ip
+			}
+		}
+		dns {
+			servers = ["9.9.9.9", "1.1.1.1",  "1.0.0.1"]
+		}
+	}
 
-  network_device {
-    bridge = "vmbr0"
-    model = "virtio"
-  }
+	network_device {
+		bridge = "vmbr0"
+		model = "virtio"
+	}
 
-  agent {
-    enabled = true
-  }
+	agent {
+		enabled = true
+	}
 
-  startup {
-    down_delay = -1
-    order      = -1
-    up_delay   = -1
-  }
+	startup {
+		down_delay = -1
+		order      = -1
+		up_delay   = -1
+	}
 
-  lifecycle {
-    ignore_changes = [
-      startup
-    ]
-  }
+	lifecycle {
+		ignore_changes = [
+			startup
+		]
+	}
 }
 
 #-------------------------------------------------------
@@ -205,94 +184,94 @@ resource "proxmox_virtual_environment_vm" "dns" {
 #-------------------------------------------------------
 // Reference https://atxfiles.netgate.com/mirror/downloads/
 resource "proxmox_virtual_environment_download_file" "pf_sense_iso_2" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pm2"
-  url          = "https://atxfiles.netgate.com/mirror/downloads/pfSense-CE-2.7.2-RELEASE-amd64.iso.gz"
-  file_name    = "pfSense-CE-2.7.2-RELEASE-amd64.iso" # rename to *.iso for import
-  overwrite    = false
-  overwrite_unmanaged = true
-  checksum = "883fb7bc64fe548442ed007911341dd34e178449f8156ad65f7381a02b7cd9e4"
-  checksum_algorithm = "sha256"
-  decompression_algorithm = "gz"
+	content_type = "iso"
+	datastore_id = "local"
+	node_name    = "pm2"
+	url          = "https://atxfiles.netgate.com/mirror/downloads/pfSense-CE-2.7.2-RELEASE-amd64.iso.gz"
+	file_name    = "pfSense-CE-2.7.2-RELEASE-amd64.iso" # rename to *.iso for import
+	overwrite    = false
+	overwrite_unmanaged = true
+	checksum = "883fb7bc64fe548442ed007911341dd34e178449f8156ad65f7381a02b7cd9e4"
+	checksum_algorithm = "sha256"
+	decompression_algorithm = "gz"
 }
 
 variable "pfs1_target" {
-  type = string
-  default = "pm2"
+	type = string
+	default = "pm2"
 }
 
 resource "proxmox_virtual_environment_vm" "pfs1" {
-  vm_id        = 110
-  name        = "pfs1"
-  node_name   = var.pfs1_target
-  description = "Managed by Terraform"
-  tags        = ["terraform"]
-  started = true
-  on_boot = true
-  reboot_after_update = true
+	vm_id        = 110
+	name        = "pfs1"
+	node_name   = var.pfs1_target
+	description = "Managed by Terraform"
+	tags        = ["terraform"]
+	started = true
+	on_boot = true
+	reboot_after_update = true
 
-  cpu {
-    cores = 1
-    type  = "host"
-  }
-  memory {
-    dedicated = 2048
-    floating  = 2048 # set equal to dedicated to enable ballooning
-  }
-  disk {
-    datastore_id = "local-lvm"
-    interface    = "scsi0"
-    discard      = "on"
-    size         = 50
-  }
-  cdrom {
-    file_id = proxmox_virtual_environment_download_file.pf_sense_iso_2.id
-  }
-  
-  initialization {
-    datastore_id = "local-lvm"
+	cpu {
+		cores = 1
+		type  = "host"
+	}
+	memory {
+		dedicated = 2048
+		floating  = 2048 # set equal to dedicated to enable ballooning
+	}
+	disk {
+		datastore_id = "local-lvm"
+		interface    = "scsi0"
+		discard      = "on"
+		size         = 50
+	}
+	cdrom {
+		file_id = proxmox_virtual_environment_download_file.pf_sense_iso_2.id
+	}
+	
+	initialization {
+		datastore_id = "local-lvm"
 
-    ip_config {
-      ipv4 {
-        address = "${var.pfs1_ip}/24"
-        gateway = var.gateway_ip
-      }
-    }
-    dns {
-      servers = var.dns_ips
-    }
-  }
+		ip_config {
+			ipv4 {
+				address = "${var.pfs1_ip}/24"
+				gateway = var.gateway_ip
+			}
+		}
+		dns {
+			servers = [for server in var.dns_server_list : server.ip_address]
+		}
+	}
 
-  network_device {
-    bridge = "vmbr0"
-    model = "virtio"
-  }
+	network_device {
+		bridge = "vmbr0"
+		model = "virtio"
+	}
 
-  network_device {
-    bridge = "vmbr1"
-    model = "virtio"
-    firewall = false
-    vlan_id = 100
-  }
+	network_device {
+		bridge = "vmbr1"
+		model = "virtio"
+		firewall = false
+		vlan_id = 100
+	}
 
-  agent {
-    enabled = true
-  }
+	agent {
+		enabled = true
+	}
 
-  startup {
-    down_delay = -1
-    order      = -1
-    up_delay   = -1
-  }
+	startup {
+		down_delay = -1
+		order      = -1
+		up_delay   = -1
+	}
 
-  lifecycle {
-    ignore_changes = [
-      started,
-      cdrom,
-      ipv4_addresses,
-      ipv6_addresses,
-      startup,
-    ]
-  }
+	lifecycle {
+		ignore_changes = [
+			started,
+			cdrom,
+			ipv4_addresses,
+			ipv6_addresses,
+			startup,
+		]
+	}
 }
