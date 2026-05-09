be8e702d65c38879c8a440467211747fb7788589
Chris Petta <cpetta@pm.me>
2026-05-09 06:57:03 -0400

    add vip failover for control planes

--- terraform/04-talos_control_plane_patch.tf
+++ terraform/04-talos_control_plane_patch.tf
@@ -8,6 +8,19 @@ locals {
         disk  = "/dev/sda"
         image = data.talos_image_factory_urls.this.urls.installer
       }
+      network = {
+        interfaces = [
+          {
+            interface = "eth0"
+            dhcp = false
+
+            # HA Layer 2 VIP configuration
+            vip = {
+              ip = "192.168.0.227"
+            }
+          }
+        ]
+      }
     }
   }
 }
